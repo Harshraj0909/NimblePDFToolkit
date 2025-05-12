c30d10657eecdb91934d6175ce8e8c5abe911b07
Harshraj Singh Rajwat <harshrajsingh7050@gmail.com>
2025-05-12 11:39:39 +0530

    Delete merge.py

--- merge.py
+++
@@ -1,7 +0,0 @@
-from PyPDF2 import PdfMerger
-def merge_pdfs(filepaths, output_path):
-    merger = PdfMerger()
-    for path in filepaths:
-        merger.append(path)
-    merger.write(output_path)
-    merger.close()
