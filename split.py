10b382193462daa031ee351fa04e7fe72f9a55ec
Harshraj Singh Rajwat <harshrajsingh7050@gmail.com>
2025-05-12 11:39:49 +0530

    Delete split.py

--- split.py
+++
@@ -1,8 +0,0 @@
-from PyPDF2 import PdfReader, PdfWriter
-def split_pdf(filepath, start, end, output_path):
-    reader = PdfReader(filepath)
-    writer = PdfWriter()
-    for page in range(start - 1, end):
-        writer.add_page(reader.pages[page])
-    with open(output_path, 'wb') as f:
-        writer.write(f)
